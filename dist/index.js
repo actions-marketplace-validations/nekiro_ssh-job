228bf5ddbcb713c037334df0cda481a5183f56c6
Marcin Jałocha <nekiro.dev@gmail.com>
2023-07-22 23:32:34 +0200

    Dont print empty buffers

--- dist/index.js
+++ dist/index.js
@@ -243,9 +243,33 @@ function run() {
             // export provided envs
             configManager.config.command.unshift(`export ${envs.map(({ key, value }) => `${key}="${value}"`).join(" ")}`);
             core.info(`Executing commands...`);
+            const errBuffer = [];
+            const outBuffer = [];
             yield ssh.execCommand(configManager.config.command.join(";"), {
-                onStdout: chunk => console.log("out:", chunk.toString("utf8")),
-                onStderr: chunk => console.log("err:", chunk.toString("utf8")),
+                onStdout: chunk => {
+                    const msg = chunk.toString("utf8").trim();
+                    if (msg.length === 0 || msg.startsWith("\n")) {
+                        console.log(`${msg.length === 0} ${msg.startsWith("\n")}, msg: ${msg}`);
+                        return;
+                    }
+                    outBuffer.push(msg);
+                    if (msg.endsWith("\n")) {
+                        console.log("out:", outBuffer.join(""));
+                        outBuffer.length = 0;
+                    }
+                },
+                onStderr: chunk => {
+                    const msg = chunk.toString("utf8").trim();
+                    if (msg.length === 0 || msg.startsWith("\n")) {
+                        console.log(`${msg.length === 0} ${msg.startsWith("\n")}, msg: ${msg}`);
+                        return;
+                    }
+                    errBuffer.push(msg);
+                    if (msg.endsWith("\n")) {
+                        console.log("err:", errBuffer.join(""));
+                        errBuffer.length = 0;
+                    }
+                },
             });
             core.info("Done executing all commands!");
         }
--- src/main.ts
+++ src/main.ts
@@ -37,9 +37,38 @@ async function run(): Promise<void> {
 
 		core.info(`Executing commands...`);
 
+		const errBuffer: string[] = [];
+		const outBuffer: string[] = [];
+
 		await ssh.execCommand(configManager.config.command.join(";"), {
-			onStdout: chunk => console.log("out:", chunk.toString("utf8")),
-			onStderr: chunk => console.log("err:", chunk.toString("utf8")),
+			onStdout: chunk => {
+				const msg = chunk.toString("utf8").trim();
+				if (msg.length === 0 || msg.startsWith("\n")) {
+					console.log(`${msg.length === 0} ${msg.startsWith("\n")}, msg: ${msg}`);
+					return;
+				}
+
+				outBuffer.push(msg);
+
+				if (msg.endsWith("\n")) {
+					console.log("out:", outBuffer.join(""));
+					outBuffer.length = 0;
+				}
+			},
+			onStderr: chunk => {
+				const msg = chunk.toString("utf8").trim();
+				if (msg.length === 0 || msg.startsWith("\n")) {
+					console.log(`${msg.length === 0} ${msg.startsWith("\n")}, msg: ${msg}`);
+					return;
+				}
+
+				errBuffer.push(msg);
+
+				if (msg.endsWith("\n")) {
+					console.log("err:", errBuffer.join(""));
+					errBuffer.length = 0;
+				}
+			},
 		});
 
 		core.info("Done executing all commands!");
